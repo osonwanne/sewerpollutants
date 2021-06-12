0f5e5467c9d3496ea068bde788c39d61e84b5480
Bon Osonwanne <nosonwan@gmail.com>
2021-06-11 23:00:14 -0700

    simple bar chart

---
+++ dashboard.py
@@ -0,0 +1,30 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html 
+import plotly.graph_objects as go
+import pandas as pd
+
+
+# Load data
+
+df = pd.read_excel('LIMS.xls',index_col=0, parse_dates=True)
+
+# Plot data
+
+app = dash.Dash()
+
+app.layout = html.Div([dcc.Graph(id='barchart',
+						figure = {'data':[
+								go.Bar(
+								x = df['U_SAMPLE_DTTM'],
+    							y = df['DISPLAYVALUE'],
+    							)], 
+						'layout':go.Layout(title='Sewer Pollutants',
+								yaxis_range=[0,2],
+								yaxis_title_text='Metals ug/L'
+								)}
+						)])
+
+
+if __name__ == '__main__':
+	app.run_server()
